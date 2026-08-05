c682e23baedf783869fbe08400f9c84bd06d798a
sonadobysar-cmd <sonadobysar@gmail.com>
2026-08-06 00:17:52 +0200

    Přidat anonymní analytiku Vini d'Elite

---
+++ vini-d-elite/js/vercel-analytics.js
@@ -0,0 +1,12 @@
+(function () {
+  window.va =
+    window.va ||
+    function () {
+      (window.vaq = window.vaq || []).push(arguments);
+    };
+
+  var script = document.createElement("script");
+  script.defer = true;
+  script.src = "/_vercel/insights/script.js";
+  document.head.appendChild(script);
+})();
